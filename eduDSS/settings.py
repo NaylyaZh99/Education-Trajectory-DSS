75f81527b42d0a3cc8d3d76d3f408d27c2c950e0
NaylyaZh99 <zh.naylya99@gmail.com>
2022-10-23 15:43:15 +0300

    Little fix

--- eduDSS/settings.py
+++ eduDSS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x%_ojj=quo8$i61&gkw^tjx5+ct(uy#f!9xlq0in3f(7q714km'
+SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
