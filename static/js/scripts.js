68bb7c6cb33a73588c4ac99a264b93046354cf5e
NaylyaZh99 <zh.naylya99@gmail.com>
2023-04-02 17:25:24 +0300

    Add static_root for prod

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,8 @@ console.log("JavaScript code running");
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-console.log("Found", coll.length, "collapsible elements");
-
 for (i = 0; i < coll.length; i++) {
-    console.log("Attaching click listener to element", coll[i]);
     coll[i].addEventListener("click", function() {
-        console.log("Button clicked!");
         this.classList.toggle("active");
         var table = document.getElementById("table_" + this.id.split("_")[1]);
         if (table.style.display === "block") {
